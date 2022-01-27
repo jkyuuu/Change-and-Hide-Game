53373babb602bfa14444b154b66676c6a3de2906
jkyuuhub <quswhdrb@naver.com>
2022-01-27 15:20:28 +0900

    오브젝트 인식 레이 발사

--- Assets/Scripts/ObjectAwareness.cs
+++ Assets/Scripts/ObjectAwareness.cs
@@ -7,16 +7,39 @@ public class ObjectAwareness : MonoBehaviour
     public Transform rayTransform;
     private float rayDistance = 50f;
 
-    //public LayerMask objectTarget;
+    public LayerMask objectTarget;
 
-    private void Click()
+
+    public void Update()
+    {
+        Awareness();
+    }
+
+    public void Awareness()
     {
         RaycastHit hit;
         Vector3 hitposition = Vector3.zero;
 
-        if(Physics.Raycast(rayTransform.position, rayTransform.forward, out hit, rayDistance))
+        int objectTarget = 1 << LayerMask.NameToLayer("ObjectTarget");
+        //objectTarget = ~objectTarget;
+
+        if(Physics.Raycast(rayTransform.transform.position, rayTransform.transform.forward, out hit, rayDistance, objectTarget))
         {
-            ObjectTarget target = hit.collider.GetComponent<ObjectTarget>();
+            //ObjectTarget target = hit.collider.GetComponent<ObjectTarget>();
+
+            //GameObject target = hit.transform.GetComponent<GameObject>();
+            //if(target != null)
+            //{
+            //    target.gameObject
+            //}
+
+            Debug.DrawRay(rayTransform.transform.position, rayTransform.transform.forward * rayDistance, Color.blue, 0.3f);
+            Debug.Log("ObjectTarget ¥Í¿Ω");
         }
+        else
+        {
+            Debug.Log("¥Í¡ˆ æ ¿Ω");
+        }
+       
     }
 }
