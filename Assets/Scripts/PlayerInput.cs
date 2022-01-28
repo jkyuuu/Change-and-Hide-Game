b9ac9df6de901185e0044b8c823c567502b85977
jkyuuhub <quswhdrb@naver.com>
2022-01-28 19:22:57 +0900

    returnPlayer 키 수정

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,19 +6,22 @@ public class PlayerInput : MonoBehaviour
 {
     public string zInputName = "Vertical";
     public string xInputName = "Horizontal";
-    public string rayButtonName = "Fire1";
+    public string selectButtonName = "Fire1";
+    
 
     // 값 할당은 내부에서만
     public float zInput { get; private set; }
     public float xInput { get; private set; }
-    public bool ray { get; private set; }
+    public bool selectObject { get; private set; }
+    public bool returnPlayer { get; private set; }
 
 
     private void Update()
     {
         zInput = Input.GetAxis(zInputName);
         xInput = Input.GetAxis(xInputName);
-        ray = Input.GetButton(rayButtonName);
+        selectObject = Input.GetButton(selectButtonName);
+        returnPlayer = Input.GetKeyDown("c");
     }
 
 }
