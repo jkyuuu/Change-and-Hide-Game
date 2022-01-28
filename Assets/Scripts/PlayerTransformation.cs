b6e70928f8498f1eedb0036f128db77d3c74aa75
jkyuuhub <quswhdrb@naver.com>
2022-01-28 19:24:38 +0900

    rayHit 변신 혹은 플레이어로 복귀

--- Assets/Scripts/PlayerTransformation.cs
+++ Assets/Scripts/PlayerTransformation.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerTransformation : MonoBehaviour
 {
+    [SerializeField]
+    private ObjectAwareness objectAwareness;
+    private PlayerInput playerInput;
+    private GameObject playerObject;
+
+    
     public enum State
     {
         Ready,
@@ -14,6 +20,56 @@ public class PlayerTransformation : MonoBehaviour
 
     private void OnEnable()
     {
-        state = State.Ready;
+        state = State.Ready;       
+    }
+    
+    private void Start()
+    {        
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        if(objectAwareness != null)
+        {
+            objectAwareness.Awareness();
+
+            Transformation();
+            
+        }
+
+        returnPlayer();
+    }
+
+    public void Transformation()
+    {
+        if (objectAwareness.rayHit)
+        {
+            if(playerInput.selectObject)
+            {
+                playerObject = this.gameObject;
+
+                GameObject hitObject = FindObjectOfType<HitObject>().gameObject;
+
+                playerObject = hitObject;
+
+                state = State.Transformation;
+
+                Debug.Log("변신 완료!");
+            }
+            
+        }        
+    }
+
+    public void returnPlayer()
+    {
+        if (playerInput.returnPlayer)
+        {
+            
+
+            state = State.Ready;
+
+            Debug.Log("플레이어로 복귀");
+        }
     }
 }
