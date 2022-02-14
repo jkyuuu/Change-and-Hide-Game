a2a2ee0de5d100c5b39c5f2f78e4f78d6042f8f0
jkyuuhub <quswhdrb@naver.com>
2022-02-15 01:49:29 +0900

    hitObject.Addcomponent

--- Assets/Scripts/PlayerTransformation.cs
+++ Assets/Scripts/PlayerTransformation.cs
@@ -11,7 +11,12 @@ public class PlayerTransformation : MonoBehaviour
 
     public GameObject hitObject;
     private Transform transformPoint;
-    
+
+    private Rigidbody objectRigid;
+    private CamController camController;
+
+    private Camera subCamera;
+        
     public enum State
     {
         Ready,
@@ -29,6 +34,8 @@ public class PlayerTransformation : MonoBehaviour
     private void Start()
     {        
         playerInput = GetComponent<PlayerInput>();
+        camController = GetComponent<CamController>();
+        subCamera = camController.subCam;
     }
 
     private void Update()
@@ -53,16 +60,11 @@ public class PlayerTransformation : MonoBehaviour
                 playerObject = this.gameObject;
 
                 CreateObject();
-                //GameObject hitObject = FindObjectOfType<HitObject>().gameObject;
                 Debug.Log("오브젝트 가져옴");
 
-
                 playerObject.SetActive(false);
-
                 state = State.Transformation;
-
                 Debug.Log("변신 완료!");
-
             }
             
         }        
@@ -72,13 +74,28 @@ public class PlayerTransformation : MonoBehaviour
     {
         transformPoint = playerObject.transform;
         hitObject = Instantiate(objectAwareness.hitGameobject, transformPoint.position, transformPoint.rotation);
+        
+        objectRigid = hitObject.GetComponent<Rigidbody>();
+        //objectRigid.constraints = RigidbodyConstraints.None;
+        objectRigid.constraints = RigidbodyConstraints.FreezeRotationX;
+        //objectRigid.constraints = RigidbodyConstraints.FreezeRotationZ;
+
+        hitObject.AddComponent<TransformedObject>();
+        //TransformedObject transformed = hitObject.GetComponent<TransformedObject>();
+        //transformed.enabled = true;
+        hitObject.AddComponent<PlayerInput>();
+        hitObject.AddComponent<PlayerTransformation>();
+        hitObject.AddComponent<PlayerMove>();
+        hitObject.AddComponent<CamController>();
+
     }
 
     public void returnPlayer()
     {
         if (playerInput.returnPlayer)
         {
-            
+            playerObject.SetActive(true);
+            hitObject.SetActive(false);
 
             state = State.Ready;
 
