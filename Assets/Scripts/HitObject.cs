96ab81cf669593ec4fc084d3f267dcd8e4b8c4bb
jkyuuhub <quswhdrb@naver.com>
2022-02-19 21:11:58 +0900

    update

--- Assets/Scripts/HitObject.cs
+++ Assets/Scripts/HitObject.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class HitObject : MonoBehaviour
 {
+    //private PlayerTransformation playerTransformation;
+    private Rigidbody objectRigid;
+    //private GameObject other;
+
     private void Awake()
     {
-        Rigidbody objectRigid = gameObject.GetComponent<Rigidbody>();
+        objectRigid = gameObject.GetComponent<Rigidbody>();
         objectRigid.constraints = RigidbodyConstraints.FreezeAll;
+    }
+    private void Start()
+    {
+        //playerTransformation = GetComponent<PlayerTransformation>();
+    }
+    private void Update()
+    {
+        //if(playerTransformation.hitObject != null)
+        //{
+        //    other = playerTransformation.gameObject;
+
+        //    other.GetComponent<PlayerInput>();
+        //    other.GetComponent<PlayerTransformation>();
+        //    other.GetComponent<PlayerMove>();
+
+        //    objectRigid.constraints = RigidbodyConstraints.None;
+        //    objectRigid.constraints = RigidbodyConstraints.FreezeRotationX;
+        //    objectRigid.constraints = RigidbodyConstraints.FreezeRotationZ;
+        //}
     }
 }
