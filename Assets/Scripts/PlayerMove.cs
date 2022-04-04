474eb15726b61ced5c4474aa4eacda9610a412e3
jkyuuhub <quswhdrb@naver.com>
2022-04-04 18:52:24 +0900

    3인칭 카메라 바라보는 방향으로 플레이어 회전(부드러운 회전 위한 Lerp() 이용)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,30 +7,39 @@ public class PlayerMove : MonoBehaviour
     private PlayerInput playerInput;
     private Rigidbody playerRigidbody;
     private PlayerTransformation playerTransformation;
-    private CamRotate camRotate;
+    private ThirdPersonCam thirdPersonCam;
 
     public float moveSpeed = 5f;
     public float jumpPower = 5f;
     private bool isJumping;
 
     private float xMouseSensitivity = 5f;
+    private float rotateSpeed = 15f;
 
     private void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         playerRigidbody = GetComponent<Rigidbody>();
         playerTransformation = GetComponent<PlayerTransformation>();
-        camRotate = GetComponent<CamRotate>();
+        thirdPersonCam = GetComponent<ThirdPersonCam>();
     }
 
     private void FixedUpdate()
     {
         Move();
         Jump();
+
+        if (playerTransformation.hitObject)
+        {
+            thirdMove();
+        }
     }
     private void Update()
     {
-        XMouseRotate();
+        if (this.gameObject.CompareTag("Player"))
+        {
+            XMouseRotate();
+        }
     }
 
     private void Move()
@@ -58,14 +67,25 @@ public class PlayerMove : MonoBehaviour
         }
     }
     private void XMouseRotate()
-    {
-        if(playerTransformation.playerObject.gameObject != null)
-        {
-            float xMouseRotate = Input.GetAxis("Mouse X") * xMouseSensitivity;
-
-            transform.Rotate(0f, xMouseRotate, 0f);
-            Debug.Log("Y축 기준 X축 이동 감지");
-        }
+    {        
+        float xMouseRotate = Input.GetAxis("Mouse X") * xMouseSensitivity;
+        
+        transform.Rotate(0f, xMouseRotate, 0f);
+        Debug.Log("Y축 기준 X축 이동 감지");        
     }
+    private void thirdMove()
+    {
+        //(방법1)
+        Vector3 playerToSubcam = playerTransformation.subCamera.transform.forward;
+        playerToSubcam.y = 0f;
+        Quaternion newRotation = Quaternion.LookRotation(playerToSubcam);
+        playerRigidbody.rotation = Quaternion.Lerp(playerRigidbody.rotation, newRotation, Time.deltaTime * rotateSpeed);
+        
+        //playerRigidbody.MoveRotation(newRotation);
 
+        ////(방법2)
+        //var offset = playerTransformation.subCamera.transform.forward;
+        //offset.y = 0f;
+        //transform.LookAt(this.gameObject.transform.position + offset * Time.deltaTime);
+    }
 }
