1fd5671c933c344ad3229f0ff9afed8d54d51755
jkyuuhub <quswhdrb@naver.com>
2022-04-20 17:37:25 +0900

    플레이어 관련

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    //private void update()
+    //{
+    //    PlayerTransformation playerTransformation = FindObjectOfType<PlayerTransformation>();
+    //    playerTransformation.create += () => PlayerPool.ReturnPool(this);
+    //}
+}
