e13b8975e0b8f754057cd9b0a524e9d37fdbeb0c
jkyuuhub <quswhdrb@naver.com>
2022-04-28 03:16:32 +0900

    데미지 관리 인터페이스

--- Assets/Scripts/IDamageable.cs
+++ Assets/Scripts/IDamageable.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public interface IDamageable
 {
     // 데미지 양, 맞은 곳, 맞은 표면 방향을 구현해야하는 인터페이스
-    void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal); 
+    void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal);
 }
