24c7a59db39510d8829752cf6259f51a86d232b6
jkyuuhub <quswhdrb@naver.com>
2022-04-28 02:57:47 +0900

    upload

---
+++ Assets/Scripts/IDamageable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable
+{
+    // 데미지 양, 맞은 곳, 맞은 표면 방향을 구현해야하는 인터페이스
+    void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal); 
+}
